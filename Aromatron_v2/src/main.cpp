13e8d766132a0027db0bbe2716219b70062f8389
Leo Keil <leolele19@gmail.com>
2021-08-09 00:50:35 +0200

    Rewrite Using Platformio in VSCode

---
+++ Aromatron_v2/src/main.cpp
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  //String test = "Dies ist ein scheiß System";
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
